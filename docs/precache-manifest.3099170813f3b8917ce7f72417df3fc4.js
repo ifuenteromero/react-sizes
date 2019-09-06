b6d8280bf3c875b3e7c6eb875877a05edf325e7c
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2019-09-06 13:23:58 +0200

    update docs folder

---
+++ docs/precache-manifest.3099170813f3b8917ce7f72417df3fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dedfcf7cd585850257ff5e19c367ab9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41ea134d3a20c48d160b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9658a38963f5554fcb91",
+    "url": "/static/js/2.f25ade6d.chunk.js"
+  },
+  {
+    "revision": "41ea134d3a20c48d160b",
+    "url": "/static/js/main.12686d0e.chunk.js"
+  },
+  {
+    "revision": "a685d15a90c193825de3",
+    "url": "/static/js/runtime~main.7f9f71c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
